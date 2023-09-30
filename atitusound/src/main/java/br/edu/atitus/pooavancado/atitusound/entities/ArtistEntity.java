4cfe44ca1c73582cc7cd257455972763e4a97b2d
Luciano Rodrigo Ferretto <prof.ferretto@yahoo.com.br>
2023-09-29 22:04:01 -0300

    Aula 08

---
+++ atitusound/src/main/java/br/edu/atitus/pooavancado/atitusound/entities/ArtistEntity.java
@@ -0,0 +1,51 @@
+package br.edu.atitus.pooavancado.atitusound.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_artist")
+public class ArtistEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID uuid;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	private String image;
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+
+}
